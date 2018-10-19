34ec2762adf1e71d7f2f26764e7bb4fb5df242a3
LilBartholomew <calle.eld@hotmail.com>
2018-10-19 15:04:51 +0200

    tja

--- src/Metoder.java
+++ src/Metoder.java
@@ -1,3 +1,23 @@
 public class Metoder {
 	
-}
+	public static void main(String[] args) {
+
+	    for (double i = 40; i >= 30; i=i-10) {
+	        double j = 130;
+	        double x = celsiusToFahrenheit(0);
+	        double y = fahrenheitToCelsius(0);
+
+	        System.out.println(i + "  " + (x + i) + " |  " + (j + i) + "  "
+	                + (y + i));
+	    }
+	}
+
+	public static double celsiusToFahrenheit(double celsius) {
+	    return (9/5) * celsius + 32 ;
+	}
+
+	public static double fahrenheitToCelsius(double fahrenheit) {
+	    return ((fahrenheit - 32)*5)/9;
+
+	 }
+	}
